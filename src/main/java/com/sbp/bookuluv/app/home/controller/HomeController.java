4b032671cf3834479fc16157504d52cdb77ff572
sik2 <devpang20@gmail.com>
2023-07-25 05:32:26 +0900

    set project

--- src/main/java/com/sbp/bookuluv/app/home/controller/HomeController.java
+++ src/main/java/com/sbp/bookuluv/app/home/controller/HomeController.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
